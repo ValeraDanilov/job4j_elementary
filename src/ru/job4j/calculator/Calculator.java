35a2633fc80d1d88d7b3552cae6b9238b5953ce2
delphiets <delfiyets@gmail.com>
2020-03-07 18:09:28 +0200

    5.8. JavaDoc. Code convention[#245234]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculator;
+/**
+ * Calculate
+ *
+ * @author Valera Danilov (parsentev@yandex.ru)
+ */
 
 public class Calculator {
+    /**
+     * Main
+     * @param args - args.
+     */
     public static void main(String[] args) {
             int one = 1;
             int two = 2;
