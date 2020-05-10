de40341e5a035999699fb3fad894e0c178550dc8
delphiets <delfiyets@gmail.com>
2020-05-10 21:06:18 +0300

    4. Сортировка номера.[#245252]

--- src/ru/job4j/collection/LexSort.java
+++ src/ru/job4j/collection/LexSort.java
@@ -1,11 +1,14 @@
 package ru.job4j.collection;
-
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(Integer.parseInt(left.substring(0, left.indexOf('.'))), Integer.parseInt(right.substring(0, right.indexOf('.'))));
+        String[] leftSplit = left.split(". ");
+        String[] rightSplit = right.split(". ");
+        int resLeft = Integer.parseInt(leftSplit[0]);
+        int resRight = Integer.parseInt(rightSplit[0]);
+        return Integer.compare(resLeft, resRight);
     }
 }
